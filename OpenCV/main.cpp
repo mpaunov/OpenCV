054dfa683f5553d099c233364332bd6dc82d2214
Martin <paunovmar@gmail.com>
2018-08-31 15:27:50 +0300

    =Added detection and measurment functionality

---
+++ OpenCV/main.cpp
@@ -0,0 +1,182 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool compareRectangles(const cv::RotatedRect& first, const cv::RotatedRect& second) {
+	return first.size.area() > second.size.area();
+}
+
+void loadcameracalibration(std::string& settingsstring, cv::Mat& cameraMatrix, cv::Mat& distcoeffs, std::vector<std::vector<cv::Point2f>>& imagePoints) {
+
+	cv::FileStorage fs2(settingsstring, cv::FileStorage::READ);
+	//method : use(type) operator on filenode.
+	int framecount = static_cast<int>(fs2["nr_of_frames"]);
+	cv::String date;
+	//second method : use filenode::operator >>
+	fs2["calibration_time"] >> date;
+	fs2["camera_Matrix"] >> cameraMatrix;
+	fs2["distortion_coefficients"] >> distcoeffs;
+	std::cout << "framecount: " << framecount << std::endl
+		<< "calibration date: " << date << std::endl
+		<< "camera Matrix: " << cameraMatrix << std::endl
+		<< "distortion coeffs: " << distcoeffs << std::endl;
+	cv::FileNode features = fs2["features"];
+	cv::FileNodeIterator it = features.begin(), it_end = features.end();
+	int idx = 0;
+	std::vector<uchar> lbpval;
+	//iterate through a sequence using filenodeiterator
+	for (; it != it_end; ++it, idx++)
+	{
+		std::cout << "feature #" << idx << ": ";
+		std::cout << "x=" << static_cast<int>((*it)["x"]) << ", y=" << static_cast<int>((*it)["y"]) << ", lbp: (";
+		//you can also easily read numerical arrays using filenode >> std::vector operator.
+		(*it)["lbp"] >> lbpval;
+		for (int i = 0; i < static_cast<int>(lbpval.size()); i++)
+			std::cout << " " << static_cast<int>(lbpval[i]);
+		std::cout << ")" << std::endl;
+	}
+
+	cv::Mat imagePtMat;
+
+	fs2["image_points"] >> imagePtMat;
+
+	for (size_t i = 0; i < imagePtMat.rows; i++)
+	{
+		imagePoints.push_back(imagePtMat.row(i));
+	}
+
+	fs2.release();
+}
+
+cv::RNG rng(12345);
+
+int main(int argc, char* argv[]) {
+
+	std::string settingsLocation = R"(e:\projects\opnecv_test\cppopencv\out_camera_data.xml)";
+
+	cv::Mat cameraMatrix;
+	cv::Mat distCoeff;
+
+	std::vector<std::vector<cv::Point2f>> imagePoints;
+
+	loadcameracalibration(settingsLocation, cameraMatrix, distCoeff, imagePoints);
+
+	cv::Mat frame;
+	cv::Mat imageGray;
+
+	cv::VideoCapture capture;
+
+	capture.open(1);
+
+	if (!capture.isOpened()) {
+		std::cerr << "error opening camera 1." << std::endl;
+		return  -1;
+	}
+
+	while (capture.read(frame)) {
+
+
+		if (frame.empty()) {
+			std::cerr << "error capturing frame" << std::endl;
+			break;
+		}
+
+		cvtColor(frame, imageGray, CV_BGR2GRAY);
+		blur(imageGray, imageGray, cv::Size(5, 5));
+
+		cv::Mat mask;
+		threshold(imageGray, mask, 50, 255, CV_THRESH_BINARY_INV);
+		cv::Canny(mask, mask, 0, 50, 3);
+		//undistort(mask, mask, cameraMatrix, distCoeff);
+		//cv::Mat outputRvec;
+		//cv::Mat outputTvec;
+		//solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeff, outputRvec, outputTvec, true, cv::SOLVEPNP_ITERATIVE);
+
+		std::vector<std::vector<cv::Point>> contours;
+		std::vector<cv::Vec4i> hierarchy;
+
+		findContours(mask, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+		//Find the rotated rectangles
+		std::vector<cv::RotatedRect> minRect(contours.size());
+
+		for (int i = 0; i < contours.size(); i++) {
+			if (cv::contourArea(contours[i]) > 2000)
+			{
+				minRect[i] = cv::minAreaRect(cv::Mat(contours[i]));
+			}
+		}
+
+		std::sort(minRect.begin(), minRect.end(), compareRectangles);
+
+		//Draw contours + rotated rectangles + ellipses
+		cv::Mat drawing = cv::Mat::zeros(mask.size(), CV_8UC3);
+
+
+		cv::Point2f points[4];
+		minRect[0].points(points);
+
+		//for (int i = 0; i < contours.size(); i++) {
+
+		cv::Scalar color = cv::Scalar(rng.uniform(0, 0), rng.uniform(0, 0), rng.uniform(250, 250));
+
+		drawContours(drawing, contours, 0, color, 3, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
+
+		//Rotated rectangle
+		cv::Point2f rect_points[4]; minRect[0].points(rect_points);
+
+		//bottomLeft, topLeft, topRight, bottomRight.
+
+		for (int j = 0; j < 4; j++) {
+			if (rect_points[j].x == 0) {
+				continue;
+			}
+			line(frame, rect_points[j], rect_points[(j + 1) % 4], color, 3, 8);
+		}
+
+		//}
+
+		//Sensor physical size
+		cv::VideoCapture cap;
+
+		if (!cap.isOpened())  // check if we succeeded
+			CV_Assert("Can't open Web-Cam");
+
+		double CamHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+		double CamWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+
+		// Function for display the object dimension
+		int imgW = 650;
+		int imgH = 50;
+		int fontFace = cv::FONT_HERSHEY_PLAIN;
+		double fontScale = 1.5;
+		int thickness = 2;
+		cv::Point textOrg(imgW / 5, imgH / 1);
+		cv::Point textOrgTwo(imgW / 5, imgH / 2.5);
+
+		std::string someText;
+
+		someText = cv::format("Height: %.2f mm", minRect[0].size.width - minRect[0].size.width * 0.28);
+		putText(frame, someText, textOrgTwo, fontFace, fontScale, cv::Scalar::all(255), thickness, 8);
+		someText = cv::format("Width: %.2f mm", minRect[0].size.height - minRect[0].size.height * 0.28);
+		putText(frame, someText, textOrg, fontFace, fontScale, cv::Scalar::all(255), thickness, 8);
+		imshow("Object Measurement", frame);
+
+		//cv::imshow("Output", mask);
+		std::string store(R"("E:\Projects\OpenCV\OpenCV\imgOutput\img.jpeg")");
+		cv::imwrite(store, frame);
+
+		const auto c = cvWaitKey(10);  //delay in milliseconds
+		if (static_cast<char>(c) > 0) {
+			break;
+		}
+	}
+
+	return 0;
+}
